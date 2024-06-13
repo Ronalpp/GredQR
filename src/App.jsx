14edf445f4ef9cb33f5631c0cde3484fbd49f5fc
ronalpp <ronalperez059@gmail.com>
2024-06-12 22:22:38 -0400

    lj

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from "./Pages/Home.jsx"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./Pages/Home.jsx";
 import Donation from './Pages/Donation.jsx';
 import Generate from './Pages/Generate.jsx';
-import NotFound from "./Pages/Notfound.jsx"
-
+import NotFound from "./Pages/Notfound.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path='/' element={<Home />} />
-        <Route path='/donation' element={<Donation />} />
-        <Route path='/generate' element={<Generate />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/donation" element={<Donation />} />
+        <Route path="/generate" element={<Generate />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
