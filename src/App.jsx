6ea8fe89a19e0b78eaec166d0d892ff4835ee528
ronalpp <ronalperez059@gmail.com>
2024-06-16 14:36:27 -0400

    before

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import Home from "./Pages/Home.jsx";
 import Donation from './Pages/Donation.jsx';
 import Generate from './Pages/Generate.jsx';
 import NotFound from "./Pages/Notfound.jsx";
-import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Analytics/>
         <Route path="/" element={<Home />} />
         <Route path="/donation" element={<Donation />} />
         <Route path="/generate" element={<Generate />} />
